4a245c977a9a2a47b194f4f9a70e2d7c732cc96e
jaron <jaronginkel@Gmail.com>
2017-02-01 01:54:39 -0600

    Shooting start.

--- Assets/Fire.cs
+++ Assets/Fire.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject bulletPrefab;
+
+    public float bulletspeed;
+
+    public Transform bulletspawn;
+
+    public float bulletlifespan;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //calls the fire command
+		if(Input.GetKeyDown(KeyCode.Space) ==true)
+        {
+            Shoot();
+        }
 	}
+
+    //method for bullet spawn
+    void Shoot()
+    {
+        GameObject bullet = (GameObject)Instantiate(bulletPrefab, bulletspawn.position, bulletspawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right*bulletspeed;
+
+        Destroy(bullet, bulletlifespan);
+    }
+
+    
 }
