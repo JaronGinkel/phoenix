8af53ff2360f97ced8ca5f56c12648eb7e7de956
jaron <jaronginkel@Gmail.com>
2017-02-02 01:04:35 -0600

    HealthBars and bullet dammage

---
+++ Assets/Scripts/UI/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour {
+
+    public RectTransform healthbar;
+
+    public static int maxhealth = 100;
+
+    public int currenthealth = maxhealth;
+	
+
+    public void TakeDammage(int ammount)
+    {
+        currenthealth -= ammount;
+        if(currenthealth<=0)
+        {
+            Destroy(gameObject);
+        }
+
+        healthbar.sizeDelta = new Vector2(currenthealth * 5/2, healthbar.sizeDelta.y);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Weapon/BulletBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BulletBehavior : MonoBehaviour {
+
+
+    public int dammage;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        GameObject hit = collision.gameObject;
+        Health health = hit.GetComponent<Health>();
+        if(health != null)
+        {
+            health.TakeDammage(dammage);
+
+        }
+        Destroy(gameObject);
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
