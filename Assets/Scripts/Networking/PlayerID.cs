32afe379441f5a5e72b1d7be02a5be96fba3a844
jaron <jaronginkel@Gmail.com>
2017-02-14 03:10:08 -0600

    Player Identity

---
+++ Assets/Scripts/Networking/PlayerID.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerID : NetworkBehaviour
+{
+
+    [SyncVar]
+    public NetworkInstanceId playerNetID;
+    private Transform myTransform;
+    [SyncVar] public string playerUniqueID;
+
+    public override void OnStartLocalPlayer()
+    {
+        playerNetID = GetComponent<NetworkIdentity>().netId;
+        playerUniqueID = playerNetID.ToString();
+    }
+}
