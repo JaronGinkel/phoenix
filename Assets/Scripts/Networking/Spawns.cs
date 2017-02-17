7663f195ad32d04392d42091eaac799af0c7eb9c
jaron <jaronginkel@Gmail.com>
2017-02-17 15:41:56 -0600

    Unique spawns

---
+++ Assets/Scripts/Networking/Spawns.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Spawns : NetworkBehaviour {
+
+    [SyncVar]
+    public NetworkInstanceId playerNetID;
+    private Transform myTransform;
+    [SyncVar]
+    public string playerUniqueID;
+    public GameObject spawn;
+    public GameObject spawn1;
+    public GameObject spawn2;
+
+    void Start () {
+        GameObject spawner = (GameObject)Instantiate(spawn);
+        if (playerUniqueID == "2")
+        {
+            this.transform.position = spawn1.transform.position;
+        }
+        else if(playerUniqueID == "3")
+        {
+            this.transform.position = spawn2.transform.position;
+        }
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+        playerNetID = GetComponent<NetworkIdentity>().netId;
+        playerUniqueID = playerNetID.ToString();
+    }
+}
